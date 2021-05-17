f526db09588b6ca8ab9dd7a6fe90021bda210805
David Jegat <david.jegat@knplabs.com>
2021-05-17 14:48:42 +0200

    Ajout de la fonction attaquer

--- public/index.php
+++ public/index.php
@@ -3,5 +3,21 @@
 require_once '../src/Personnage.php';
 
 $merlin = new Personnage();
+$arthur = new Personnage();
 
-var_dump($merlin);
+$merlin->vie = 40;
+$merlin->attaque = 20;
+
+$merlin->regenerer(80);
+
+$arthur->attaquer($merlin);
+$merlin->attaquer($arthur);
+
+$arthur->vie = 100;
+$arthur->attaque = 50;
+
+$merlin->afficher();
+
+echo '<br />';
+
+$arthur->afficher();
--- src/Personnage.php
+++ src/Personnage.php
@@ -5,4 +5,23 @@ class Personnage
     public int $vie = 100;
 
     public int $attaque = 20;
+
+    public function regenerer(int $vie = 100): void
+    {
+        $this->vie = $vie;
+    }
+
+    public function afficher(): void
+    {
+        echo 'Le personnage a '
+            . $this->vie
+            . ' de vie et '
+            . $this->attaque
+            . ' d\'attaque';
+    }
+
+    public function attaquer(Personnage $victim): void
+    {
+        $victim->vie = $victim->vie - $this->attaque;
+    }
 }
