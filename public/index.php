1becc6169f607d24819ffce1794eeed97ea4e76b
David Jegat <david.jegat@knplabs.com>
2021-05-18 09:24:41 +0200

    Ajout de l'exercice de formulaire

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
+// Require notre class de Form
+require '../src/Form.php';
 
-echo 'Welcome';
+// Créer un login form
+$loginForm = new Form();
+?>
+<html>
+
+<head>
+    <title>Mon Blog</title>
+</head>
+
+<body>
+    <form method="post">
+        <?php
+        // Afficher le label pour le nom d'utilisateur        
+        // Afficher l'input pour le nom d'utilisateur
+        // Affihcer le label pout le mot de passe
+        // Afficher l'input pour le mote de passe
+        // afficher un bouton de soumission
+        ?>
+    </form>
+</body>
+
+</html>
