70eb03d9559049920dce0fb7ddc3a13baf577d1d
David Jegat <david.jegat@knplabs.com>
2021-05-18 11:14:05 +0200

    Ajout de types

--- src/Form.php
+++ src/Form.php
@@ -2,17 +2,17 @@
 
 class Form
 {
-    public function label($text, $for): void
+    public function label(string $text, string $for): void
     {
         echo '<label for="' . $for . '">' . $text . '</label>';
     }
 
-    public function input($name, $type): void
+    public function input(string $name, string $type): void
     {
         echo '<input type="' . $type . '" name="' . $name . '" id="' . $name . '" />';
     }
 
-    public function submitButton($text): void
+    public function submitButton(string $text): void
     {
         echo '<button type="submit">' . $text . '</button>';
     }
