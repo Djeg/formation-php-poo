953e0083ac4d80d7b73aeaf1b314abb21471b3ad
David Jegat <david.jegat@gmail.com>
2021-06-01 16:43:52 +0200

    Remove commentaire

--- public/index.php
+++ public/index.php
@@ -4,14 +4,6 @@ require_once('../src/Personnage.php');
 require_once('../src/Magicien.php');
 require_once('../src/Chevalier.php');
 
-// 1. Créer un fichier dans src: Chevalier.php
-// 2. À l'intérieur de Chavalier.php nous créons la 
-//    class Chevalier qui hérite de Personnage
-// 3. On rédéfinie la méthode "attaque" de notre classe
-//    chevalier pour effectuer 20 dégat de plus !
-// 4. Dans public/index.php nous assignons $arthur à une
-//    classe chavalier
-
 $merlin = new Personnage("Merlin l'enchanteur", 50, 40);
 $arthur = new Chevalier("Le roi Arthur", 20, 30);
 $morganne = new Magicien("Morganne la sorcière", 150, 40);
