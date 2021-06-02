09531af3990ad93b7474e5a914c549f527984e95
David Jegat <david.jegat@gmail.com>
2021-06-02 14:16:11 +0200

    Suppresion de PDO en dépendance

--- src/Table/PersonnageTable.php
+++ src/Table/PersonnageTable.php
@@ -4,13 +4,6 @@ namespace App\Table;
 
 class PersonnageTable
 {
-    private \PDO $pdo;
-
-    public function __construct(\PDO $pdo)
-    {
-        $this->pdo = $pdo;
-    }
-
     public function enregistrer(array $data): void
     {
     }
