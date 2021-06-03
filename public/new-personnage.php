514ebc5b20fc9de31c5af41790150a0fa5bcad83
David Jegat <david.jegat@gmail.com>
2021-06-03 14:25:00 +0200

    Ajout de PersonnageController::newPersonnage

--- public/new-personnage.php
+++ public/new-personnage.php
@@ -2,52 +2,7 @@
 
 require(__DIR__ . '/../vendor/autoload.php');
 
-$perso = new App\Model\Personnage(
-    isset($_POST['nom']) ? $_POST['nom'] : '',
-    isset($_POST['vie']) ? (int)$_POST['vie'] : 0,
-    isset($_POST['attaque']) ? (int)$_POST['attaque'] : 0,
-    isset($_POST['magie']) ? (int)$_POST['magie'] : 0
-);
+use App\Controller\PersonnageController;
 
-if (!empty($_POST)) {
-    $validateur = new App\Validateur\PersonnageValidateur();
-    $table = new App\Model\Table\PersonnageTable($validateur);
-    $errors = $table->enregistrer($perso);
-}
-
-?>
-<form action="./new-personnage.php" method="post">
-    <div>
-        <?php if (isset($errors['nom'])) { ?>
-            <p><?php echo $errors['nom']; ?></p>
-        <?php } ?>
-        <label for="nom">
-            Nom du personnage :
-        </label>
-        <input type="text" name="nom" id="nom" value="<?php echo $perso->getNom(); ?>">
-    </div>
-    <div>
-        <?php if (isset($errors['vie'])) { ?>
-            <p><?php echo $errors['vie']; ?></p>
-        <?php } ?>
-        <label for="vie">Vie du personnage :</label>
-        <input type="number" name="vie" id="vie" value="<?php echo $perso->getVie(); ?>">
-    </div>
-    <div>
-        <?php if (isset($errors['attaque'])) { ?>
-            <p><?php echo $errors['attaque']; ?></p>
-        <?php } ?>
-        <label for="attaque">Attaque du personnage :</label>
-        <input type="number" name="attaque" id="attaque" value="<?php echo $perso->getAttaque(); ?>">
-    </div>
-    <div>
-        <?php if (isset($errors['magie'])) { ?>
-            <p><?php echo $errors['magie']; ?></p>
-        <?php } ?>
-        <label for="magie">Magie du personnage :</label>
-        <input type="number" name="magie" id="magie" value="<?php echo $perso->getMagie(); ?>">
-    </div>
-    <div>
-        <button type="submit">Créer le personnage</button>
-    </div>
-</form>
+$controller = new PersonnageController();
+$controller->newPersonnage();
--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Model\Personnage;
+use App\Model\Table\PersonnageTable;
+use App\Validateur\PersonnageValidateur;
+
 class PersonnageController
 {
     public function index(): void
@@ -10,5 +14,19 @@ class PersonnageController
 
     public function newPersonnage(): void
     {
+        $perso = new Personnage(
+            isset($_POST['nom']) ? $_POST['nom'] : '',
+            isset($_POST['vie']) ? (int)$_POST['vie'] : 0,
+            isset($_POST['attaque']) ? (int)$_POST['attaque'] : 0,
+            isset($_POST['magie']) ? (int)$_POST['magie'] : 0
+        );
+
+        if (!empty($_POST)) {
+            $validateur = new PersonnageValidateur();
+            $table = new PersonnageTable($validateur);
+            $errors = $table->enregistrer($perso);
+        }
+
+        include __DIR__ . '/../View/newPersonnage.php';
     }
 }
