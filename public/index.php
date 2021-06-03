1cf3d3ca59da6e4f3cafd4f06db90d621c3eebcc
David Jegat <david.jegat@gmail.com>
2021-06-03 14:50:30 +0200

    Correction exercice

--- public/index.php
+++ public/index.php
@@ -2,33 +2,7 @@
 
 require(__DIR__ . '/../vendor/autoload.php');
 
-$merlin = new App\Personnage("Merlin l'enchanteur", 50, 40);
-$arthur = new App\Chevalier("Le roi Arthur", 20, 30);
-$morganne = new App\Magicien("Morganne la sorcière", 150, 40);
-$lancelot = new App\Chevalier("Lancelot du lac");
+use App\Controller\PersonnageController;
 
-echo $arthur->afficher();
-echo "<br />";
-echo $merlin->afficher();
-echo "<br />";
-echo $morganne->afficher();
-
-echo "<br />";
-echo "Arthur attaque merlin !";
-echo "<br />";
-
-$arthur->attaque($merlin); // Merlin vie === 20 (50 - 30)
-
-echo $arthur->afficher();
-echo "<br />";
-echo $merlin->afficher();
-
-echo "<br />";
-echo "Morganne soigne merlin !";
-echo "<br />";
-
-$morganne->soigne($merlin);
-
-echo $arthur->afficher();
-echo "<br />";
-echo $merlin->afficher();
+$controller = new PersonnageController();
+$controller->index();
