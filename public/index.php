e2e7b0d1e4a80ebdd59a4b83466a372ada1ef85e
David Jegat <david.jegat@gmail.com>
2021-06-03 15:03:48 +0200

    Add a frontend controller

--- public/index.php
+++ public/index.php
@@ -4,5 +4,7 @@ require(__DIR__ . '/../vendor/autoload.php');
 
 use App\Controller\PersonnageController;
 
+$pageName = isset($_GET['page']) ? $_GET['page'] : 'index';
+
 $controller = new PersonnageController();
-$controller->index();
+$controller->$pageName();
--- public/new-personnage.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require(__DIR__ . '/../vendor/autoload.php');
-
-use App\Controller\PersonnageController;
-
-$controller = new PersonnageController();
-$controller->newPersonnage();
