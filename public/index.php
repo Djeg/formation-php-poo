16edac11d57889482629c9baf074ac2738e61348
David Jegat <david.jegat@gmail.com>
2022-11-07 12:11:49 +0100

    Typo

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // On inclue l'autoload composer nous permettant d'utiliser nos classes
-require __DIR__ . '/../autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // On test si l'on demande un fichier se terminant par les extensions suivante
 if (preg_match("/(.css|.png|.jpeg|.js|.gif|.pdf|.mp3|.mp4|.html|.jpg)$/i", $_SERVER['REQUEST_URI'])) {
