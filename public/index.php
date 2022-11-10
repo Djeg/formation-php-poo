ebe314a3425bd74921eb340514d441c173075452
David Jegat <david.jegat@gmail.com>
2022-11-10 10:17:52 +0100

    Ajout du container

--- public/index.php
+++ public/index.php
@@ -2,10 +2,11 @@
 
 // On inclue l'autoload composer nous permettant d'utiliser nos classes
 
-use App\Core\Configuration;
 use App\Core\View;
-use App\Table\ArticleTable;
+use App\Core\Container;
 use App\Table\UserTable;
+use App\Core\Configuration;
+use App\Table\ArticleTable;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -16,36 +17,13 @@ if (preg_match("/(.css|.png|.jpeg|.js|.gif|.pdf|.mp3|.mp4|.html|.jpg)$/i", $_SER
     return false;
 }
 
-// Créer une View
-$view = new View(__DIR__ . '/../templates');
-
-// Créer la configuration
-$config = new Configuration(__DIR__ . '/..');
-
-echo "<p>Est-ce que DATABASE_USER existe ?</p>";
-echo "<p>{$config->has('DATABASE_USER')}</p>";
-
-// Affiche le contenue de DATABASE_USER
-echo $config->get('DATABASE_USER');
-
-// Affiche le « template » (la page) hello.php
-echo $view->render('hello', ['name' => 'John Doe']);
-
-// Je souhaiterais récupérer tout les utilisateurs :
-// 1. Se connécter à la base de données (créer un objet PDO
-$pdo = new PDO(
-    "mysql:host={$config->get('DATABASE_HOST')};
-        port={$config->get('DATABASE_PORT')};
-        dbname={$config->get('DATABASE_NAME')}",
-    $config->get('DATABASE_USER'),
-    $config->get('DATABASE_PASSWORD'),
-);
-// 2. Créer un objet UserTable
-$table = new UserTable($pdo);
-// 3. On appel la méthode findAll
-$users = $table->findAll();
+// Création du container
+$container = Container::boot();
 
 echo "<h2>Les utilisateurs</h2>";
+
+// récupére tout les utilisateurs
+$users = $container->get(UserTable::class)->findAll();
 
 foreach ($users as $user) {
     echo "<p>{$user->email}</p>";
@@ -53,11 +31,8 @@ foreach ($users as $user) {
 
 // J'aimerais afficher tout les articles !!
 
-// 1. Créer l'objet ArticleTable
-$articleTable = new ArticleTable($pdo);
-
 // 2. On récupére tout les articles
-$articles = $articleTable->findAll();
+$articles = $container->get(ArticleTable::class)->findAll();
 
 echo "<h2>Les Articles</h2>";
 
---
+++ src/Core/Container.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use PDO;
+use Exception;
+use App\Table\UserTable;
+use App\Table\ArticleTable;
+
+/**
+ * Le container est un objet contenant tout les services
+ * de notre application. Un service est une instance d'une classe
+ * 
+ * Cet objet contient TOUTES les instances que notre application
+ * a besoin pour fonctionner.
+ */
+class Container
+{
+    protected array $services;
+
+    /**
+     * Démarre un container avec TOUT les services
+     * de l'application
+     */
+    static public function boot(): Container
+    {
+        $container = new Container();
+
+        return $container
+            // Chemin vers le répertoire racine du projet
+            ->add('ROOT_DIR', __DIR__ . '/../..')
+            // Chemin vers le répertoire des templates
+            ->add('TEMPLATE_DIR', "{$container->get('ROOT_DIR')}/templates")
+            // Création de l'objet View
+            ->add(View::class, new View($container->get('TEMPLATE_DIR')))
+            // Création de l'objet Configuration
+            ->add(Configuration::class, new Configuration($container->get('ROOT_DIR')))
+            // Création de l'objet PDO
+            ->add(PDO::class, new PDO(
+                "mysql:host={$container->get(Configuration::class)->get('DATABASE_HOST')};
+                    port={$container->get(Configuration::class)->get('DATABASE_PORT')};
+                    dbname={$container->get(Configuration::class)->get('DATABASE_NAME')}",
+                $container->get(Configuration::class)->get('DATABASE_USER'),
+                $container->get(Configuration::class)->get('DATABASE_PASSWORD'),
+            ))
+            // Création de la table des utilisateurs
+            ->add(UserTable::class, new UserTable($container->get(PDO::class)))
+            // Création de la table des articles
+            ->add(ArticleTable::class, new ArticleTable($container->get(PDO::class)));
+    }
+
+    public function __construct()
+    {
+        $this->services = [];
+    }
+
+    /**
+     * Est ce que un service éxiste
+     */
+    public function has(string $name): bool
+    {
+        return isset($this->services[$name]);
+    }
+
+    /**
+     * Ajoute un service dans le container
+     */
+    public function add(string $name, $service): self
+    {
+        if ($this->has($name)) {
+            throw new Exception("Oops, il semble que le service $name soit dèja enregistré");
+        }
+
+        $this->services[$name] = $service;
+
+        return $this;
+    }
+
+    /**
+     * Retourne un service
+     */
+    public function get(string $name): mixed
+    {
+        if (!$this->has($name)) {
+            throw new Exception("Oops, il semble que le service $name ne soit pas enregistré. Peut-être que c'est simplement une faute de frappe ?");
+        }
+
+        return $this->services[$name];
+    }
+}
